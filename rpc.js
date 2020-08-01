ee15b732d77ac7584b18833ee3dbbb6e6e6c7739
Julie <MsMutinda@users.noreply.github.com>
2020-08-01 18:58:29 +0300

    Commit

--- rpc.js
+++ rpc.js
@@ -1,21 +1,46 @@
-//Play game function
+//initiate game
+const game = () => {
+    let playerScore = 0;
+    let compScore = 0;
+    const play = document.querySelector('.match button');
+    const match = document.querySelector('.match');
+
+    play.addEventListener('click', () => {
+        play();
+    });
+};
+
+//Play game 
 const play = () => {
-    const option = document.querySelector('.options button');
+    const options = document.querySelector('.options button');
     const player = document.querySelector('.you');
     const computer = document.querySelector('.Computer');
     //randomly generate the computer's options
     const compOptions = ['Rock', 'Paper', 'Scissors'];
-    options.forEach(option => {
-        option.addEventListener('click', function() {
+    for(var i=0; i < compOptions.length; i++) {
+        var self = compOptions[i];
+        self.addEventListener("click", function(event) {
+        event.preventDefault();
+        compPlay(this);
+        }, false());
+    }
+
+    const compPlay = () => {
             const random = Math.floor(Math.random()*3);
             //math.random generates a number between 0 and 1 so we multiply the result by 3
             //math.floor() rounds down the generated number
             const compChoice = compOptions[random];
-            scores();
-        });
-    });
-};
+            scores(this.textContent, compChoice);
+            //update hand images
+            player.src = `./Rock-Paper-Scissors/${this.textContent}.png`;
+            comp.src = `./Rock-Paper-Scissors/${compChoice}.png`;
+        };
+}
 
+//update scores
+
+
+//track scores
 const scores = (playerChoice, compChoice) => {
     const winner = document.querySelector('.start');
     if(playerChoice === compChoice) {
@@ -33,7 +58,6 @@ const scores = (playerChoice, compChoice) => {
             return;
             }
         }
-    }
 
     //check for paper
     if(playerChoice === 'Paper'){
@@ -58,7 +82,7 @@ const scores = (playerChoice, compChoice) => {
             return;
             }
         }
-
+    }
 //call functions
 play();
 scores();
